b64a772df86c1f1319498ef409640f0a69ac9946
Shahzad <shazee257@gmail.com>
2023-08-31 00:42:05 +0500

    post image on fb

--- app.js
+++ app.js
@@ -1,42 +1,77 @@
 const express = require('express');
 require('dotenv').config();
-const SocialPost = require("social-post-api");
+const axios = require("axios");
 
 const app = express();
-const { API_KEY } = process.env;
-const social = new SocialPost(API_KEY);
 
-// post on fb page
-// multiple images in premium plan
-// schedule post in premium plan
-app.post('/fb-page-post', async (req, res) => {
-    const post = await social.post({
-        "post": "Today is a great day, test 5!",
-        // "scheduleDate": "2023-08-30T12:30:00Z",
-        "platforms": ["facebook"],
-        "mediaUrls": ["https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"],
-    }).catch(console.error);
+const { FB_PAGE_TOKEN, FB_PAGE_ID } = process.env;
 
-    res.json(post);
+// facebook page (single image post)
+app.post("/fb-page-post", async (req, res) => {
+    const response = await axios.post(`https://graph.facebook.com/${FB_PAGE_ID}/photos?access_token=${FB_PAGE_TOKEN}`, {
+        "message": "Post 6 from Node.js API",
+        "url": "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg"
+    });
 
+    if (!response) {
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+
+    res.json(response.data);
 });
 
-// post on facebook group
-// you can post in your created group only
-app.post('/fb-group-post', async (req, res) => {
-    const post = await social.post({
-        "post": "Today is a great day, test 5!",
-        "platforms": ["fbg"],
-        "mediaUrls": [
-            "https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+// facebook page (multiple posts)
+app.post("/fb-posts", async (req, res) => {
+    const photoUrls = [
+        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
+        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
+        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg"
+    ];
 
-        ],
-    }).catch(console.error);
+    const photoUploadPromises = photoUrls.map(async (url) => {
+        const response = await axios.post(`https://graph.facebook.com/${FB_PAGE_ID}/photos?access_token=${FB_PAGE_TOKEN}`, {
+            "message": "Post from Node.js API",
+            "url": url
+        });
 
-    res.json(post);
+        return response.data;
+    });
 
+    try {
+        const uploadResults = await Promise.all(photoUploadPromises);
+        res.json(uploadResults);
+    } catch (error) {
+        console.error("Error uploading photos:", error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
 });
 
+app.post("/fb-post-urls", async (req, res) => {
+    const photoUrls = [
+        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
+        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
+        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg"
+    ];
 
+    const mediaAttachments = photoUrls.map((url) => {
+        return {
+            media_type: "IMAGE",
+            url: url
+        };
+    });
+
+    const postData = {
+        message: "Post from Node.js API with multiple images",
+        attached_media: mediaAttachments
+    };
+
+    try {
+        const response = await axios.post(`https://graph.facebook.com/${FB_PAGE_ID}/feed?access_token=${FB_PAGE_TOKEN}`, postData);
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error posting with multiple images:", error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+});
 
 module.exports = app;
