7a0e1a364cb566718452e432bb896f15e4b671f2
Shahzad <shazee257@gmail.com>
2023-09-02 03:08:24 +0500

    implemented via FB library

--- app.js
+++ app.js
@@ -1,77 +1,64 @@
 const express = require('express');
 require('dotenv').config();
+const FB = require('fb');
 const axios = require("axios");
 
 const app = express();
-
 const { FB_PAGE_TOKEN, FB_PAGE_ID } = process.env;
+
+FB.setAccessToken(FB_PAGE_TOKEN);
+
+
+
 
 // facebook page (single image post)
 app.post("/fb-page-post", async (req, res) => {
-    const response = await axios.post(`https://graph.facebook.com/${FB_PAGE_ID}/photos?access_token=${FB_PAGE_TOKEN}`, {
-        "message": "Post 6 from Node.js API",
-        "url": "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg"
+    FB.api('me/photos', 'post', {
+        message: "Test Post from Node.js API",
+        // link: "www.google.com",
+        url: "https://images.unsplash.com/photo-1693370268702-92402dd47484?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
+    }, (result) => {
+        if (!result || result.error) {
+            console.log(!result ? 'error occurred' : result.error);
+            return res.status(500).json({ error: "Something went wrong" });
+        }
+        console.log('Post Id: ' + result.id);
+        res.json(result.id);
     });
 
-    if (!response) {
-        return res.status(500).json({ error: "Something went wrong" });
-    }
 
-    res.json(response.data);
+    // "message": "Image Post from Node.js API",
+    // "url": "https://images.unsplash.com/photo-1693370268702-92402dd47484?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
+    //     "message": "Video Post from Node.js API",
+    //     "link": "www.google.com",
+    //     "source": "https://images.unsplash.com/photo-1693370268702-92402dd47484?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
+    //     // "url": "https://peacemakers3.s3.us-east-2.amazonaws.com/uploads/6e8ff526-3879-444b-9ebc-834d8ad41412-monarch_-_327%20(720p).mp4"
+
+    //     return res.status(500).json({ error: "Something went wrong" });
+    // }
+
+    // res.json(response.data);
+
+    // const response = await axios.post(`https://graph.facebook.com/${FB_PAGE_ID}/me/photos?access_token=${FB_PAGE_TOKEN}`, {
+    //     // "message": "Image Post from Node.js API",
+    //     // "url": "https://images.unsplash.com/photo-1693370268702-92402dd47484?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
+    //     "message": "Video Post from Node.js API",
+    //     "link": "www.google.com",
+    //     "source": "https://images.unsplash.com/photo-1693370268702-92402dd47484?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
+
+    //     // "url": "https://peacemakers3.s3.us-east-2.amazonaws.com/uploads/6e8ff526-3879-444b-9ebc-834d8ad41412-monarch_-_327%20(720p).mp4"
+
+    // });
+
+    // if (!response) {
+    //     return res.status(500).json({ error: "Something went wrong" });
+    // }
+
+    // res.json(response.data);
 });
 
-// facebook page (multiple posts)
-app.post("/fb-posts", async (req, res) => {
-    const photoUrls = [
-        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
-        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
-        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg"
-    ];
 
-    const photoUploadPromises = photoUrls.map(async (url) => {
-        const response = await axios.post(`https://graph.facebook.com/${FB_PAGE_ID}/photos?access_token=${FB_PAGE_TOKEN}`, {
-            "message": "Post from Node.js API",
-            "url": url
-        });
 
-        return response.data;
-    });
 
-    try {
-        const uploadResults = await Promise.all(photoUploadPromises);
-        res.json(uploadResults);
-    } catch (error) {
-        console.error("Error uploading photos:", error);
-        res.status(500).json({ error: "Something went wrong" });
-    }
-});
-
-app.post("/fb-post-urls", async (req, res) => {
-    const photoUrls = [
-        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
-        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg",
-        "https://litterguru-web.techup.sg/assets/images/reality/reality-2-after.jpeg"
-    ];
-
-    const mediaAttachments = photoUrls.map((url) => {
-        return {
-            media_type: "IMAGE",
-            url: url
-        };
-    });
-
-    const postData = {
-        message: "Post from Node.js API with multiple images",
-        attached_media: mediaAttachments
-    };
-
-    try {
-        const response = await axios.post(`https://graph.facebook.com/${FB_PAGE_ID}/feed?access_token=${FB_PAGE_TOKEN}`, postData);
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error posting with multiple images:", error);
-        res.status(500).json({ error: "Something went wrong" });
-    }
-});
 
 module.exports = app;
